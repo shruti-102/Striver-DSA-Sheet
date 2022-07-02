29a901e7d96786557e012ac9282faf4af3b56a2c
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-07-03 01:41:47 +0530

    Create Merge Intervals.cpp

---
+++ Day 2/Merge Intervals.cpp
@@ -0,0 +1,27 @@
+//time o(n)
+//space o(n)
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> res;
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        
+        for(int i=1;i<intervals.size();i++)
+        {
+            if(end >= intervals[i][0])
+            {
+                end=max(end,intervals[i][1]);
+            }
+            else
+            {
+                res.push_back({start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
+            }
+        }
+        res.push_back({start,end});
+        return res;
+    }
+};
